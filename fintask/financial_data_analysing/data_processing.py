475d713ba1ba2414434990da2b6cb8e3e11e1606
Amirhossein <anejadkoorki8199@gmail.com>
2021-11-15 10:36:15 +0330

    plot updated.

--- fintask/financial_data_analysing/data_processing.py
+++ fintask/financial_data_analysing/data_processing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import plotly.graph_objects as go
+from datetime import datetime
 
 
 def financial_data_display(input_file):
@@ -10,5 +12,16 @@ def financial_data_display(input_file):
 
 
 def financial_data_plot(input_file):
-    data = pd.read_csv(input_file)
-    return data.plot(kind='line', x='<VOL>', y='<CLOSE>')
+    df = pd.read_csv(input_file)
+    df["<DTYYYYMMDD>"] = df["<DTYYYYMMDD>"].astype('str')
+    df["<DTYYYYMMDD>"] = pd.to_datetime(df["<DTYYYYMMDD>"], format="%Y-%m-%d")
+    fig = go.Figure(data=[go.Candlestick(x=df["<DTYYYYMMDD>"],
+                    open=df['<FIRST>'],
+                    high=df['<HIGH>'],
+                    low=df['<LOW>'],
+                    close=df['<CLOSE>'])
+                          ])
+    fig.update_layout(xaxis_rangeslider_visible=False)
+    return fig.show()
+
+
--- fintask/financial_data_analysing/views.py
+++ fintask/financial_data_analysing/views.py
@@ -19,18 +19,18 @@ def base_page_view(request):
             if "plot" in request.POST:
                 plot = financial_data_plot(excel_file)
                 # plt.show()
-                buffer = io.BytesIO()
-                plt.savefig(buffer, format='png')
-                buffer.seek(0)
-                image_png = buffer.getvalue()
-                buffer.close()
-                graphic = base64.b64encode(image_png)
-                graphic = graphic.decode('utf-8')
+                # buffer = io.BytesIO()
+                # plt.savefig(buffer, format='png')
+                # buffer.seek(0)
+                # image_png = buffer.getvalue()
+                # buffer.close()
+                # graphic = base64.b64encode(image_png)
+                # graphic = graphic.decode('utf-8')
                 return render(
                     request,
                     template_name="financial_data_analysing/base_page.html",
                     context={
-                        'graphic': graphic
+                        'graphic': plot
                     }
                 )
             else:
